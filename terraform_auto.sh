e4ac6a749c17564cb5c73ed051e0776c3f34cb37
otubu-ramune <purius0701@gmail.com>
2021-10-17 21:21:18 +0900

    remove unneccesary echo

--- terraform_auto.sh
+++ terraform_auto.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
 COUNT=1
-echo error $?
 terraform apply -auto-approve
 while [ $? -eq 1 ]
 do
-  echo error $?
   echo try $COUNT
   sleep 10
   COUNT=$((COUNT+1))
   echo tera start
   terraform apply -auto-approve
 done
-echo don-e
 echo Resource Created! >> result.txt
